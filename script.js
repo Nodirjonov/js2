cb9fed9a63fb9a43bceb1ff9ed446761a4725c57
Nodirjonov <sobirovakomila85@gmail.com>
2024-02-25 21:56:21 +0500

    js2

---
+++ script.js
@@ -0,0 +1,11 @@
+let a=prompt("Ismingizni kiriting")
+let b=prompt("Familiyangizni kiriting")
+let c=+prompt("tug'ulgan kunizni kiriting")
+
+
+alert(`siz kiritgan ${a} ${typeof a}
+siz kiritgan ${b} ${typeof b}
+siz kiritgan ${c} ${typeof c}
+`)
+const result = confirm('Haqiqatan ham bu faylni oʻchirib tashlamoqchimisiz?');
+
